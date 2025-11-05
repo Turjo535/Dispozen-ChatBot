2a87f24c2cb742292bfef92fd698a0eae42e6270
sifat31 <sifat.rahman31@gmail.com>
2025-11-05 13:57:30 +0600

    change for docker

--- DispozenProject/settings.py
+++ DispozenProject/settings.py
@@ -152,7 +152,7 @@ DATABASES = {
         "NAME": "dispozen_eventmanagement",
         "USER": "postgres",
         "PASSWORD": "turjo35",
-        "HOST": "127.0.0.1",
+        "HOST": "db",
         "PORT": "5432",
     }
 }
